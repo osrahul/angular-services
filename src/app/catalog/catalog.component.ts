84b2721860fc6e8dfc7eef88ce76c209e6d08d00
rahul.rajbanshi <rahul.rajbanshi@verisk.com>
2024-06-23 23:12:19 +0545

    dependency injection

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Product } from './product.model';
 import { ProductService } from './product.service';
-import { CartService } from '@core/cart.service';
+import { CART_SERVICE_TOKEN, CartService } from '@core/cart.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,7 +12,9 @@ import { Observable } from 'rxjs';
 export class CatalogComponent {
   products: Observable<Product[]> = this.productService.getProducts();
 
-  constructor(private productService: ProductService, private cartService: CartService) { }
+  constructor(
+    private productService: ProductService,
+    @Inject(CART_SERVICE_TOKEN) private cartService: CartService) { }
 
   addToCart(product: Product) {
     this.cartService.add(product);
